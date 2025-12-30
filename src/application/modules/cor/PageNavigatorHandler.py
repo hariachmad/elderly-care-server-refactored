14040ca0b27b943296ff8dd547f1614349c39b78
hariachmad <hari08achmad@gmail.com>
2025-12-30 17:11:36 +0700

    modify : pageNavigatorHandler

--- src/application/modules/cor/PageNavigatorHandler.py
+++ src/application/modules/cor/PageNavigatorHandler.py
@@ -9,8 +9,10 @@ class PageNavigatorHandler(Handler):
         super().__init__()
     def handle(self, state)->bool:
         intent = state["intent"]
+        path = INTENT_TO_NODE[intent]
+        for key in state["date"]:
+            path = path+"/"+str(state["date"][key])
         if intent not in UNNAVIGATOR_INTENT:
-            path = INTENT_TO_NODE[intent]
             self.sio.instance.emit("navigateCommand",f"/{path}")
             if super().handle(state) is None:
                 return state
